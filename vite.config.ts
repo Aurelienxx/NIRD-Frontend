4cf0642b980f16c35b60569c38ea33e3910625eb
Aurélienxx <aurelienfontaine09@gmail.com>
2026-06-04 09:20:45 +0200

    CHORE : dump

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
 
   preview: {
     host: true,
     port: 3000,
-    allowedHosts: true
+    allowedHosts: [
+      'nird.jrcan.dev'
+    ]
   }
 })
